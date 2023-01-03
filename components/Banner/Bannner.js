2b796525f98eb89d13a2337ea249a81331fe00b8
kmdshojib <kmd.shojib@gmail.com>
2023-01-04 01:05:47 +0600

    project added

---
+++ components/Banner/Bannner.js
@@ -0,0 +1,14 @@
+import style from './banner.module.css'
+
+const Bannner = ({buttonText,handleOnClick}) => {
+
+    return (
+        <div className={`${style.container} mt-10 ml-10`}>
+            <h1 className={`${style.title} flex mb-5`}><span className={style.title1}>Coffee</span> <span className={style.title2}>Connoisseur</span></h1>
+            <p className={`${style.subTitle} mb-5`}>Discover Shop Nareby!</p>
+            <button className={style.button} onClick={handleOnClick}>{buttonText}</button>
+        </div>
+    )
+}
+
+export default Bannner
---
+++ pages/coffee-store.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CoffeeSotre = () => {
+  return (
+    <div>CoffeeSotre</div>
+  )
+}
+
+export default CoffeeSotre
