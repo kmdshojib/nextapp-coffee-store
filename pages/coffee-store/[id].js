e58988d22fe32676e9a8a863cd2cf93258bf4484
kmdshojib <kmd.shojib@gmail.com>
2023-01-10 15:32:15 +0600

    dynamic route added

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -1,9 +1,35 @@
 import Link from "next/link"
 import { useRouter } from "next/router"
 
-const CoffeeSotre = () => {
+import cofffeeStoresData from "../../data/coffee-stores.json"
+
+export const getStaticProps = (staticprops) =>{
+  const params = staticprops.params
+  return {
+    props:{
+      cofffeeStores:cofffeeStoresData.find(store => {
+        console.log(store)
+        return store.id === 0
+      })
+    }
+  }
+}
+
+export const getStaticPaths = () =>{
+  return {
+    paths:[{params: {id: "0"}}],
+    fallback: false,
+  }
+}
+
+const CoffeeSotre = (props) => {
   const router = useRouter()
+
   console.log("router",router)
+
+
+  console.log(props)
+
   return (
     <div>CoffeeSotre <Link href="/">Home</Link></div>
   )
